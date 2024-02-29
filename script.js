4aabc9b49bfd44314a1fda395908c89d3565ff33
SAIMOON <100423426+saimoon777c@users.noreply.github.com>
2024-02-29 07:43:20 -0800

    My Websites{}

---
+++ script.js
@@ -0,0 +1,4 @@
+function toggleMenu() {
+    var menuContent = document.querySelector('.menu-content');
+    menuContent.style.display = (menuContent.style.display === 'block') ? 'none' : 'block';
+}
